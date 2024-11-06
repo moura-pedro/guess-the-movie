21ef5d0e8a58397346f6444d16f1b8adc8d776f3
Pedro Moura <latiphy2812@gmail.com>
2024-11-05 22:34:18 -0500

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/guess-the-movie/',  // Replace with your repository name
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
